31a875db15eefb62afb0d2ff6619482500a0388b
jewtechx <jklarbi_danquah@st.ug.edu.gh>
2024-02-27 07:13:25 +0000

    feat(apartment): Implemented file upload system

--- file_upload_engine/src/schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const imageSchema = new mongoose.Schema({
-    filename:{type:String,required:true},
-    path:{type:String,required:true}
-})
-
-const Image = mongoose.model('image',imageSchema)
-module.exports = Image
